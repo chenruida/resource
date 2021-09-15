f5d5b9796e19491d75cbbf0123306f68b91c5ee7
chenruida <chenruida@outlook.com>
2021-09-15 09:37:15 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import {Card,Row,Col} from 'element-ui';
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
+Vue.use(Card);
+Vue.use(Row);
+Vue.use(Col);
+Vue.prototype.$axios = axios
+Vue.prototype.$axios.defaults.baseURL = 'http://127.0.0.1:3000'
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
