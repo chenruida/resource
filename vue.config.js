8467ae4edb4f293811d3860aca05eac4a2507938
Chenrd <chenruida@outlook.com>
2021-09-29 10:13:32 +0800

    fix config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: "./", // 公共路径(必须有的)
-  outputDir: "/var/www/resource ", // 输出文件目录
+  outputDir: "/var/www/resource", // 输出文件目录
   assetsDir: "static", //静态资源文件名称
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
@@ -10,11 +10,4 @@ module.exports = {
   },
   lintOnSave: true,
   productionSourceMap: false, //去除打包后js的map文件
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "./src/assets/css/common.scss";`,
-      },
-    },
-  },
 };
